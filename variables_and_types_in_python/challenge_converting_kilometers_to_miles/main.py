ff21e17882e67dfcfbade588f0a1bda9a234fa92
yervant2008 <>
2026-07-16 16:24:47 +0000

    Challenge: Converting Kilometers to Miles

---
+++ variables_and_types_in_python/challenge_converting_kilometers_to_miles/main.py
@@ -0,0 +1,12 @@
+# Distance in kilometers
+distance_in_kilometers = 4677.4
+
+# Convert distance to miles and store in `distance_in_miles`
+distance_in_miles = distance_in_kilometers / 1.609
+
+# Convert variable type to int and store in `int_distance_in_miles`
+int_distance_in_miles = int(distance_in_miles)
+
+# Testing
+print("San Francisco - New York distance (float): ", distance_in_miles, "mi")
+print("San Francisco - New York distance (int): ", int_distance_in_miles, "mi")
