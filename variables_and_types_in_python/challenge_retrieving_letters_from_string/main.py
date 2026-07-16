3e2449c241a6b74f069ae16f0d50fdc4f4fa4762
yervant2008 <>
2026-07-16 17:37:54 +0000

    Challenge: Retrieving Letters from String

---
+++ variables_and_types_in_python/challenge_retrieving_letters_from_string/main.py
@@ -0,0 +1,16 @@
+# Initial string to demonstrate string manipulation
+sample_text = "This is a string for learning"
+
+# Extracting the first character from the string
+first_char = sample_text[0]
+
+# Extracting the last character from the string
+last_char = sample_text[-1]
+
+# Extracting a highlighted word from the string
+highlighted_word = sample_text[-12:-9]
+
+# Testing
+print("The first char of the string: ", first_char)
+print("The last char of the string: ", last_char)
+print("The highlighted word from the string: ", highlighted_word)
